502e3e31616de84a10fb0aba20cc0d48e27ff9e1
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-31 20:55:34 +0000

    refactored snake game with keys in place

--- input.js
+++ input.js
@@ -1,19 +1,34 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection; 
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-setupInput();
 
-const handleUserInput = function(key) {
+//setupInput();
+
+const handleUserInput = stdin.on('data', (key) => {
+  if (key === "w") {
+    conn.write('Move: up');
+  }
+  if (key === "a") {
+    conn.write("Move: right");
+  }
+  if (key === "s") {
+    conn.write("Move: down");
+  }
+  if (key === "d") {
+    conn.write("Move: left");
+  }
   if (key === '\u0003') {
     process.exit();
   }
+});
+return stdin;
+}
+module.exports = {
+  setupInput
 };
-
-module.exports = { setupInput };
