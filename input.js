524328b3df294c5b73cd1a9122d789687620e29f
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-31 19:13:44 +0000

    refactored

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+setupInput();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,8 +1,6 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
+const { setupInput } = require('./input');
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+setupInput();
